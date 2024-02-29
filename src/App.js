630f9050198c921de22131d5b638916aedb0acde
ThaysaRafaele <thaysarafaele@gmail.com>
2024-02-28 20:37:16 -0400

    feat: removendo trecho comentado

--- src/App.js
+++ src/App.js
@@ -6,7 +6,6 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        {/* <img src={logo} className="App-logo" alt="logo" /> */}
         <div>
           <h1>Exemplo de Estilização de componentes</h1>
           <Container/>
